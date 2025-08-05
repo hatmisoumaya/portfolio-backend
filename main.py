a723d754a84506f6e1fa9c0df99a5f43dd2f5582
EL HATMI SOUMAYA <122816497+hatmisoumaya@users.noreply.github.com>
2025-08-05 12:31:38 +0100

    commit

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests, os
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+prompt_content = os.getenv("PROMPT_TEXT")  
+
+app = FastAPI()
+
+class Question(BaseModel):
+    message: str
+
+@app.post("/ask")
+def ask_user(question: Question):
+    headers = {
+        "Authorization": f"Bearer {GROQ_API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "model": "llama-3.1-8b-instant",
+        "temperature": 0.9,
+        "messages": [
+            {
+            "role": "system",
+            "content": prompt_content  # Now this is guaranteed to be valid
+        },
+        {
+            "role": "user",
+            "content": question.message
+        }
+        ]
+    }
+
+    response = requests.post("https://api.groq.com/openai/v1/chat/completions", json=payload, headers=headers)
+    data = response.json()
+    reply = data["choices"][0]["message"]["content"]
+    return {"response": reply}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
